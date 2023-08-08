8082ca4e1d78c3ee5d9c45158d48c1e5894f0b9e
lex <savchenko.lex@gmail.com>
2023-08-08 04:50:05 +0300

    default data and new section for search

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import ListContacts from './components/ListContacts/ListContacts';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter:'',
     name: '',
     number: '',
   };
@@ -46,6 +52,9 @@ class App extends Component {
             nameValue={this.state.name}
             telValue={this.state.number}
           />
+        </Section>
+        <Section title="Quick Search by name:">
+          
         </Section>
         <Section title="Contacts">
           <ListContacts contacts={this.state.contacts} />
