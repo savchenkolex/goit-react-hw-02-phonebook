0ff28e6311ce906f22b0cb5f31ca353dce7ca5b5
lex <savchenko.lex@gmail.com>
2023-08-08 20:56:16 +0300

    minor fix. add filter-input in css

--- src/components/QuickSearch/QuickSearch.jsx
+++ src/components/QuickSearch/QuickSearch.jsx
@@ -1,14 +1,17 @@
-import css from "./QuickSearch.module.css";
+import css from './QuickSearch.module.css';
 
-export default function QuickSearch (props) {
-    return (
-        <form autoComplete="off" >
-            <label>
-                <span>Quick search: </span>
-            <input type="text" name="filter" onChange={event => {
-                props.fnInput(event);
-            }} />
-            </label>
-        </form>
-    )
+export default function QuickSearch(props) {
+  return (
+    <form autoComplete="off">
+        <input
+            className={css["filter-input"]}
+          type="text"
+          name="filter"
+          onChange={event => {
+            props.fnInput(event);
+          }}
+          placeholder='Type for quick search'
+        />
+    </form>
+  );
 }
